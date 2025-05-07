ecdf777c2b26dba80f7c1a7acaa7214d6c82e3ff
cloudreaping <125616423+cloudreaping@users.noreply.github.com>
2025-05-07 17:54:41 -0400

    Update gradebook.js

--- Public/gradebook.js
+++ Public/gradebook.js
@@ -11,7 +11,7 @@ function fetchGradeData() {
     // Check if we're done
     if(xhr.readyState === xhr.DONE){
       // Check if we're successful
-      if(xhr.status !== 2000){
+      if(xhr.status !== 200){
         console.error(`Could not get grades.
           status: ${xhr.status}`);
       }
@@ -30,14 +30,14 @@ function populateGradebook(data) {
   // This function will take the fetched grade data and populate the table
   console.log("Populating gradebook with data:", data);
   let tableElm = document.getElementByID("gradebook"); // Get the gradebook table element
-    Data.forEach(function(assignment){ // For each row of data we're passed in
+    data.forEach(function(assignment){ // For each row of data we're passed in
       let row = document.createElement("tr"); // create a table row element
       let columns = []; // Handy place to stick the columns of information
       columns.name = document.createElement('td'); //the first columns table data will be the name
       columns.name.appendChild(
         // Just put the name in text, you could be fancy and figure out the letter grade here 
         // with either a bunch of conditions or a JavaScript "switch" statement
-        document.createTextNode(Assignment.total_grade)
+        document.createTextNode(assignment.total_grade)
         );
       // add the table data columns to the table row
       row.appendChild(columns.name);
